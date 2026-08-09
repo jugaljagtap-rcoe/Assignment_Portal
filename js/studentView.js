e3d4ad66185bb0b2455d0dd2cb43902d6f809315
rcoe-fe-results <hod.humanities@eng.rizvi.edu.in>
2026-08-09 22:22:07 +0530

    Fix syntax error in studentView.js submission history map

--- js/studentView.js
+++ js/studentView.js
@@ -858,7 +858,7 @@ const studentView = {
                     </td>
                     <td style="font-size:11px; color:var(--text-secondary);">${new Date(s.submittedAt).toLocaleString()}</td>
                   </tr>
-                `).join('')
+                `; }).join('')
               }
             </tbody>
           </table>
